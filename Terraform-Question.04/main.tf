75bddb90380245c8cca4e74bba803bcc4e815cd7
avin025 <74015201+avin025@users.noreply.github.com>
2021-07-13 10:27:31 +0530

    Update main.tf

--- Terraform-Question.04/main.tf
+++ Terraform-Question.04/main.tf
@@ -17,3 +17,12 @@ module "Nginx" {
   key_name = "${module.key.key_name}"
 }
 
+  module "node-js" {
+  source    = "modules/EC2"
+  count     = 1
+  ins_type  = "t2.micro"
+  ami       = "${lookup(var.ec2_ami_ubuntu,var.region)}"
+  subnet_id = "${module.vpc.private_subnet_id}"
+  key_name  = "${module.key.key_name}"
+}
+
